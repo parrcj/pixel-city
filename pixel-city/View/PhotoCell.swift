96c730bcc5b56110f90fe91444c62bc799511b0a
Chris Parr <parr.cj@gmail.com>
2019-02-17 12:22:52 +0000

    programmatic add of collection view

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCellCollectionViewCell.swift
+//  pixel-city
+//
+//  Created by Christopher Parr on 17/02/2019.
+//  Copyright © 2019 Christopher Parr. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
