71c86db7a57ee9ebe9f30e709c970f4961ec5285
Chris Parr <parr.cj@gmail.com>
2019-02-18 10:28:55 +0000

    present popvc to view each image.

---
+++ pixel-city/Controller/PopVC.swift
@@ -0,0 +1,38 @@
+//
+//  PopVC.swift
+//  pixel-city
+//
+//  Created by Christopher Parr on 18/02/2019.
+//  Copyright © 2019 Christopher Parr. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    var passedImage: UIImage!
+    
+    func initData(forImage image: UIImage){
+        self.passedImage = image
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popImageView.image = passedImage
+        addDoubleTap()
+    }
+    
+    func addDoubleTap(){
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
